6f3c72ec8e91b5d9314a064362fb34c5ef1e3985
sofiane <abdellaoui.sofiane.esb@gmail.com>
2023-06-09 18:53:25 +0200

    setup flask-email

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, render_template, redirect, url_for, session
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -22,6 +23,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# email
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+mail = Mail(app)
 
 
 # shell
