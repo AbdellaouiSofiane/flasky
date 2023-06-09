297950ff4f35ac97aadf6c3e6b21b61b3ffd2043
sofiane <abdellaoui.sofiane.esb@gmail.com>
2023-06-09 14:19:03 +0200

    add NameForm to index page

--- hello.py
+++ hello.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+import os
 
+from flask import Flask, render_template, redirect, url_for, session
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+# app initialization
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+# app config
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hard to guess string')
 
-@app.route('/')
+
+# forms
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
