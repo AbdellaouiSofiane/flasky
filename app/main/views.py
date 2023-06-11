53f6124deec2eb2473a3eaa10121c7ed53cdc12a
sofiane <abdellaoui.sofiane.esb@gmail.com>
2023-06-11 17:04:04 +0200

    remove dummy code

--- app/main/views.py
+++ app/main/views.py
@@ -1,36 +1,7 @@
-from flask import session, redirect, render_template, url_for, current_app
-
-from .. import db
-from ..models import User
-from ..email import send_email
-
+from flask import render_template
 from . import main
-from .forms import NameForm
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    form = NameForm()
-    name = session.get('name')
-    known = session.get('known')
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if not user:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'],
-                           'New User', 'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=name,
-                           known=known)
-
-
-@main.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+    return render_template('index.html')
