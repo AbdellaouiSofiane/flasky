c6483e464aa27e1be189ca52bce25b9b5869323c
sofiane <abdellaoui.sofiane.esb@gmail.com>
2023-06-15 14:11:26 +0200

    add profile page

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
 db = SQLAlchemy()
+moment = Moment()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 
@@ -21,6 +23,7 @@ def create_app(config_name):
     mail.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    moment.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
