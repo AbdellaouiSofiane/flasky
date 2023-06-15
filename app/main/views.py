2dfb0b773be0e044b4f3d4bf6bc0e8a94195fe56
sofiane <abdellaoui.sofiane.esb@gmail.com>
2023-06-15 14:38:13 +0200

    add profile edit page

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_required, current_user
+from .forms import EditProfileForm
 from . import main
-from ..models import User
+from ..models import User, db
 
 
 @main.route('/')
@@ -12,3 +14,18 @@ def index():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    return render_template('edit-profile.html', form=form)
